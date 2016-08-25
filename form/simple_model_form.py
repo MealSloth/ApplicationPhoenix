3cf165165acb3bf91f0ea86973a39921ed67fa09
Michael <michael@mealsloth.com>
2016-08-24 17:00:36 -0700

    Project page stuff and adding some neat things to Phoenix as well as get_model and update_model generic stuff in NCSUCKI

--- form/simple_model_form.py
+++ form/simple_model_form.py
@@ -2,10 +2,16 @@ from django.forms.models import ModelForm
 
 
 class SimpleModelForm(ModelForm):
-    def process(self):
-        params = {}
+    def process_create(self):
+        kwargs = {}
         for field in self.Meta.fields:
-            params[field] = self.cleaned_data[field]
-        create_method = getattr(self.Meta.model, 'create_%s' % self.Meta.model.__name__.lower())
-        result = create_method(kwargs=params)
+            kwargs[field] = self.cleaned_data[field]
+        create_method = getattr(self.Meta.model.objects, 'create_%s' % self.Meta.model.__name__.lower())
+        result = create_method(**kwargs)
         return result
+
+    def process_update(self, instance):
+        for field in self.Meta.fields:
+            setattr(instance, field, self.cleaned_data[field])
+        instance.save()
+        return instance
