7b1f389c61f2b693a8f2cca692e03710aa02f596
Michael <michael@mealsloth.com>
2016-08-27 22:54:28 -0700

    Some project and post stuff. Simple post edit form with not submit functionality atm

---
+++ static/js/phoenix.js
@@ -0,0 +1,10 @@
+/**
+ * Created by michael on 8/27/16.
+ */
+(function (Phoenix) {
+    Phoenix.Tooltips = {
+        Init: function () {
+            $(".phoenix-tooltip").tooltip();
+        }
+    };
+})(window.Phoenix || (window.Phoenix = {}));
---
+++ static/js/phoenix.ready.js
@@ -0,0 +1,6 @@
+/**
+ * Created by michael on 8/27/16.
+ */
+$(function () {
+    Phoenix.Tooltips.Init();
+});
